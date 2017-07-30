8d611ea14d499ca983119bbeb7ca8a1c4bfba0e8
teofiluscandra <teofiluschandra@gmail.com>
2017-07-30 23:45:34 +0800

    make it better

--- config.js
+++ config.js
@@ -3,3 +3,10 @@ export const google = {
   clientSecret: 'HDVkOZBxBABqETue-scL8sJZ',
   callbackURL: 'https://boogaloo-oauth.herokuapp.com/auth/google/callback',
 };
+
+export const facebook = {
+  clientID: '1855814468023418',
+  clientSecret: '7a4a2cbb85e4b3a4a4926f1d83427c89',
+  callbackURL: 'https://boogaloo-oauth.herokuapp.com/auth/facebook/callback',
+  profileFields: ['id', 'name', 'displayName', 'picture', 'email'],
+};
--- index.js
+++ index.js
@@ -2,8 +2,7 @@ import express from 'express';
 import passport from 'passport';
 import FacebookStrategy from 'passport-facebook';
 import GoogleStrategy from 'passport-google-oauth20';
-import { google } from './config';
-
+import { facebook, google } from './config';
 
 // Transform Google profile into user object
 const transformGoogleProfile = (profile, accessToken) => ({
@@ -13,6 +12,18 @@ const transformGoogleProfile = (profile, accessToken) => ({
   accessToken : accessToken
 });
 
+const transformFacebookProfile = (profile) => ({
+  name: profile.name,
+  avatar: profile.picture.data.url,
+});
+
+// Register Facebook Passport strategy
+passport.use(new FacebookStrategy(facebook,
+  // Gets called when user authorizes access to their profile
+  async (accessToken, refreshToken, profile, done)
+    // Return done callback and pass transformed user object
+    => done(null, transformFacebookProfile(profile._json))
+));
 
 
 // Register Google Passport strategy
@@ -35,13 +46,21 @@ const app = express();
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Set up Facebook auth routes
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/auth/facebook' }),
+  // Redirect user back to the mobile app using Linking with a custom protocol OAuthLogin
+  (req, res) => res.redirect('OAuthLogin://login?user=' + JSON.stringify(req.user)));
+
+
 // Set up Google auth routes
 app.get('/auth/google', passport.authenticate('google', { scope: ['profile','email'] }));
 
 app.get('/auth/google/callback',
   passport.authenticate('google', { failureRedirect: '/auth/google' }),
   (req, res) => {
-        
         res.redirect('OAuthLogin://login?user=' + JSON.stringify(req.user))
     });
 
