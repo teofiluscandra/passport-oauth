d450ca014df45f11ba8c9b26651726bfbfcf507c
teofiluscandra <teofiluschandra@gmail.com>
2017-07-30 22:29:49 +0800

    make it better

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const google = {
   clientID: '905049240281-qdh3hbm269f4uiltfd69buaagsvtsvo5.apps.googleusercontent.com',
   clientSecret: 'HDVkOZBxBABqETue-scL8sJZ',
-  callbackURL: 'http://localhost:3000/auth/google/callback',
+  callbackURL: 'https://boogaloo-oauth.herokuapp.com/auth/google/callback',
 };
