f5f4ba7d87301695a494e0a168166a0f29372683
Teofilus Candra <teofiluschandra@gmail.com>
2018-03-04 14:21:51 +0800

    Facebook and Google

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 export const google = {
-  clientID: '905049240281-qdh3hbm269f4uiltfd69buaagsvtsvo5.apps.googleusercontent.com',
-  clientSecret: 'HDVkOZBxBABqETue-scL8sJZ',
-  callbackURL: 'https://boogaloo-oauth.herokuapp.com/auth/google/callback',
+  clientID: '',
+  clientSecret: '',
+  callbackURL: '',
 };
 
 export const facebook = {
-  clientID: '1855814468023418',
-  clientSecret: '7a4a2cbb85e4b3a4a4926f1d83427c89',
-  callbackURL: 'https://boogaloo-oauth.herokuapp.com/auth/facebook/callback',
+  clientID: '',
+  clientSecret: '',
+  callbackURL: '',
   profileFields: ['id', 'name', 'displayName', 'picture', 'email', 'age_range']
 };
